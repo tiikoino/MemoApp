21de4b821eaa0f806933aad7d2c3710eb65a22d5
tiikoino <oukakanose@gmail.com>
2014-12-22 15:35:03 +0900

    演習テキストと同期

---
+++ MemoApp/src/java/sample/javaee/memoapp/bean/MemoEditBean.java
@@ -0,0 +1,42 @@
+package sample.javaee.memoapp.bean;
+
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.faces.context.FacesContext;
+import javax.faces.context.Flash;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import sample.javaee.memoapp.ejb.MemoFacade;
+import sample.javaee.memoapp.entity.Memo;
+
+@Named(value = "memoEditBean")
+@ViewScoped
+public class MemoEditBean implements Serializable {
+
+    private Memo editMemo;
+
+    @Inject
+    private MemoFacade memoFacade;
+
+    @PostConstruct
+    public void init() {
+        Flash flash = FacesContext.getCurrentInstance()
+                .getExternalContext().getFlash();
+        Integer key = (Integer) flash.get("editMemoId");
+        editMemo = memoFacade.find(key);
+    }
+
+    public void updateMemo() {
+        memoFacade.edit(editMemo);
+    }
+
+    public Memo getEditMemo() {
+        return editMemo;
+    }
+
+    public void setEditMemo(Memo editMemo) {
+        this.editMemo = editMemo;
+    }
+    
+}
